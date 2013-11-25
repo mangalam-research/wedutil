6cf33116bc5ee2a6f37f30323d7b7f1c475e0731
Louis-Dominique Dubeau <ldd@lddubeau.com>
2013-11-24 19:19:44 -0500

    Initial.

---
+++ README.rst
@@ -0,0 +1,2 @@
+This repository contains a python package used for running the
+Selenium tests for `wed <https://github.com/mangalam-research/wed>`_.
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="wedutil",
+    version="0.1.0",
+    packages=find_packages(),
+    author="Louis-Dominique Dubeau",
+    author_email="ldd@lddubeau.com",
+    description="Utilities for testing wed with Selenium.",
+    license="MPL 2.0",
+    keywords=["wed", "selenium", "testing"],
+    url="https://github.com/mangalam-research/wedutil",
+    #use_2to3=True,
+    classifiers=[
+        "Programming Language :: Python :: 2.7",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: POSIX",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance"
+    ],
+)
