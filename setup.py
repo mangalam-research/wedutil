385d0987df207709e1c54ed0ea46d9ef99477123
Louis-Dominique Dubeau <ldd@lddubeau.com>
2016-03-17 10:19:38 -0400

    New version.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="wedutil",
-    version="0.1.0",
+    version="0.19.0",
     packages=find_packages(),
     author="Louis-Dominique Dubeau",
     author_email="ldd@lddubeau.com",
